6150faa8a25a10206be1ee53973ae9869638f473
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-08-16 16:10:14 +0800

    penyesuain migration untuk user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('alamat')->nullable(); // Alamat
+            $table->string('jenis_kelamin', 10)->nullable(); // Jenis Kelamin (Pria/Wanita)
+            $table->string('nomor_hp', 15)->nullable(); // Nomor HP
             $table->rememberToken();
             $table->timestamps();
         });
