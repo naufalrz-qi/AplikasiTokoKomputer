52a76bdaf3b7e72c5adb62cf0c0c35ef82cde45d
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-08-19 01:36:29 +0800

    conflicted

--- resources/views/templates/user/home.blade.php
+++ resources/views/templates/user/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>Daftar Barang</h1>
 
+<<<<<<< Updated upstream
     <div class="product-container">
         @foreach ($barangs as $barang)
             <div class="product-card">
@@ -15,4 +16,19 @@
             </div>
         @endforeach
     </div>
+=======
+<div>
+    @foreach($barangs as $barang)
+        <div class="product-card">
+            <img src="{{ asset($barang->gambar) }}" alt="{{ $barang->nama_barang }}" class="product-image">
+            <h2>{{ $barang->nama_barang }}</h2>
+            <p>{{ $barang->merek }}</p>
+            <p>Rp {{ number_format($barang->harga, 2, ',', '.') }}</p>
+            <p>{{ Str::limit($barang->rincian_barang, 100) }}</p>
+            <a href="{{ route('barang.show', $barang->id_barang) }}">Lihat Detail</a>
+            <a href="{{ route('keranjang.create', $barang->id_barang) }}">Tambahkan ke Keranjang</a>
+        </div>
+    @endforeach
+</div>
+>>>>>>> Stashed changes
 @endsection
