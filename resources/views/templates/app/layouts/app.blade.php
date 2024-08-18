1f0f38d26e8d8848014384001563f9a5a40b11e5
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-08-19 01:42:31 +0800

    test

--- resources/views/templates/app/layouts/app.blade.php
+++ resources/views/templates/app/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User</title>
+    <title>MTC Mitra Komputer</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
