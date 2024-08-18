d1f106ada9f10db41b99eeecb25cba44c380be48
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2024-08-19 01:40:47 +0800

    fix conflict

--- resources/views/templates/user/home.blade.php
+++ resources/views/templates/user/home.blade.php
@@ -3,20 +3,6 @@
 @section('content')
     <h1>Daftar Barang</h1>
 
-<<<<<<< Updated upstream
-    <div class="product-container">
-        @foreach ($barangs as $barang)
-            <div class="product-card">
-                <img src="{{ asset($barang->gambar) }}" alt="{{ $barang->nama_barang }}" class="product-image">
-                <h2 class="product-name">{{ $barang->nama_barang }}</h2>
-                <p class="product-brand">{{ $barang->merek }}</p>
-                <p class="product-price">Rp {{ number_format($barang->harga, 2, ',', '.') }}</p>
-                <p class="product-details">{{ Str::limit($barang->rincian_barang, 100) }}</p>
-                <a href="{{ route('barang.show', $barang->id_barang) }}" class="product-link">Lihat Detail</a>
-            </div>
-        @endforeach
-    </div>
-=======
 <div>
     @foreach($barangs as $barang)
         <div class="product-card">
@@ -30,5 +16,4 @@
         </div>
     @endforeach
 </div>
->>>>>>> Stashed changes
 @endsection
